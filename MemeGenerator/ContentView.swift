20a4b9c95371275015e17acb094ec1813bf22234
Brandon Baars <brandonbaars@gmail.com>
2020-10-26 12:33:20 -0400

    Initial Commit

---
+++ MemeGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemeGenerator
+//
+//  Created by Brandon Baars on 10/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemeGenerator/MemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeGeneratorApp.swift
+//  MemeGenerator
+//
+//  Created by Brandon Baars on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct MemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
